7adfc9bf3da0b8564bccaef4bf91121faf668983
Khayati Asrani <devinc@TheDen-3.local>
2018-12-29 21:35:06 +0530

    Updates

---
+++ precache-manifest.2cd73c1ae608055454df9b3cecb38c0e.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "32758bdad56db8076c07",
+    "url": "/pomodoro-clock-pwa/static/css/main.d533d84b.chunk.css"
+  },
+  {
+    "revision": "32758bdad56db8076c07",
+    "url": "/pomodoro-clock-pwa/static/js/main.32758bda.chunk.js"
+  },
+  {
+    "revision": "7985b6a2ca3a56fd137a",
+    "url": "/pomodoro-clock-pwa/static/js/1.7985b6a2.chunk.js"
+  },
+  {
+    "revision": "8292530593a04826f656",
+    "url": "/pomodoro-clock-pwa/static/js/2.82925305.chunk.js"
+  },
+  {
+    "revision": "e16035592ffdfdb04172",
+    "url": "/pomodoro-clock-pwa/static/css/3.b725372d.chunk.css"
+  },
+  {
+    "revision": "e16035592ffdfdb04172",
+    "url": "/pomodoro-clock-pwa/static/js/3.e1603559.chunk.js"
+  },
+  {
+    "revision": "0aff6ee921133826ed11",
+    "url": "/pomodoro-clock-pwa/static/js/runtime~main.0aff6ee9.js"
+  },
+  {
+    "revision": "e65d96d04378217b8b5caff2fb1b62cb",
+    "url": "/pomodoro-clock-pwa/static/media/fa-regular-400.e65d96d0.woff2"
+  },
+  {
+    "revision": "fc5252e568503bfb163233ec63b8a2ce",
+    "url": "/pomodoro-clock-pwa/static/media/fa-regular-400.fc5252e5.woff"
+  },
+  {
+    "revision": "8e9f0ee6d016526776c27f91fea55cca",
+    "url": "/pomodoro-clock-pwa/static/media/fa-regular-400.8e9f0ee6.eot"
+  },
+  {
+    "revision": "deb8619e67992d3fa088b5dcbb8cff36",
+    "url": "/pomodoro-clock-pwa/static/media/fa-regular-400.deb8619e.ttf"
+  },
+  {
+    "revision": "b89c6c51aa2999159a6667f97a46a346",
+    "url": "/pomodoro-clock-pwa/static/media/fa-brands-400.b89c6c51.woff2"
+  },
+  {
+    "revision": "8086e197694282b26c7b729945ecc377",
+    "url": "/pomodoro-clock-pwa/static/media/fa-solid-900.8086e197.woff2"
+  },
+  {
+    "revision": "dd4ff1196905f0585aef2c80486869e9",
+    "url": "/pomodoro-clock-pwa/static/media/fa-brands-400.dd4ff119.woff"
+  },
+  {
+    "revision": "52b8345b6d8e11988d8db917c366b4da",
+    "url": "/pomodoro-clock-pwa/static/media/fa-solid-900.52b8345b.woff"
+  },
+  {
+    "revision": "7d43b9ae0b8bb1d0dc425d78dc577b0a",
+    "url": "/pomodoro-clock-pwa/static/media/fa-brands-400.7d43b9ae.eot"
+  },
+  {
+    "revision": "50a544b5ad4d512e8db3cdc089823a00",
+    "url": "/pomodoro-clock-pwa/static/media/fa-brands-400.50a544b5.ttf"
+  },
+  {
+    "revision": "734248d027cbaac7f03c0d931ec95593",
+    "url": "/pomodoro-clock-pwa/static/media/fa-regular-400.734248d0.svg"
+  },
+  {
+    "revision": "f72d546abcc27880adf8428792c72519",
+    "url": "/pomodoro-clock-pwa/static/media/fa-solid-900.f72d546a.eot"
+  },
+  {
+    "revision": "1709a2810c3752483683f3f7d287aa89",
+    "url": "/pomodoro-clock-pwa/static/media/fa-solid-900.1709a281.ttf"
+  },
+  {
+    "revision": "fef43c1fd872ef7f1880483d9b63ccf2",
+    "url": "/pomodoro-clock-pwa/static/media/fa-brands-400.fef43c1f.svg"
+  },
+  {
+    "revision": "27603739fe78fee587c7b84873475b61",
+    "url": "/pomodoro-clock-pwa/static/media/fa-solid-900.27603739.svg"
+  },
+  {
+    "revision": "977500aa00ac414e489be9c7dcc0b881",
+    "url": "/pomodoro-clock-pwa/index.html"
+  }
+];
